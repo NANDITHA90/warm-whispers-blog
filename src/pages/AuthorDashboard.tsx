b274bda5d045afad47d7dd7cfe0ac8198f6881a6
NANDITHA90 <85455463+NANDITHA90@users.noreply.github.com>
2025-07-14 17:51:39 +0530

    Updated AuthorDashboard.tsx

--- src/pages/AuthorDashboard.tsx
+++ src/pages/AuthorDashboard.tsx
@@ -111,7 +111,7 @@ const AuthorDashboard = () => {
                 Don't have any posts yet?
               </p>
               <Link to="/create" className="btn-accent">
-                <Plus className="h-4 w-5 mr-2" />
+                <Plus className="h-4 w-[600px] mr-2" />
                 Write Your First Story
               </Link>
             </div>
